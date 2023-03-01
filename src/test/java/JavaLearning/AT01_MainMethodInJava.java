e32f6f19f1cb75301c2f2607fe566a7fa1211988
softwaretestlab <softwaretestlab@hotmail.com>
2023-03-02 09:44:06 +1100

    JavaLearning

---
+++ src/test/java/JavaLearning/AT01_MainMethodInJava.java
@@ -0,0 +1,13 @@
+//package
+package JavaLearning;
+//class
+public class AT01_MainMethodInJava {
+// method -- > main method
+    public static void main(String[] args){
+        //inside method we write code
+        // this below code will print on console
+        System.out.println("This is my First Java Class");
+        System.out.println("This is Java Learning");
+    }
+
+}
---
+++ src/test/java/JavaLearning/AT02_JunitMethods.java
@@ -0,0 +1,16 @@
+package JavaLearning;
+
+import org.junit.Test;
+
+public class AT02_JunitMethods {
+    @Test
+    public void test01(){
+        System.out.println("This is Junit Method-1");
+    }
+// annotations
+    @Test
+    public void test02(){
+        System.out.println("This is Junit Method-2");
+    }
+
+}
